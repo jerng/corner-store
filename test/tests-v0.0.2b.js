86047df3f02940d6a3846394172ebcd794140f1e
Jerng <yangjerng@gmail.com>
2020-02-29 07:31:47 +0800

    Improved Examination Results report

---
+++ test/tests-v0.0.2b.js
@@ -0,0 +1,23 @@
+import * as Serl from   '../lib/serl.js'
+import * as SSON from   '../lib/sson/sson.js'
+import * as Exam from '../lib/classes/exam.js'
+
+new Exam.Exam ( { concerns : [ 
+
+/* Templates: 
+
+{   test : ``,
+    code : function () {
+    }
+},
+
+{   test : ``,
+    expectError : true,
+    code : function () {
+    }
+},
+
+{   warning : ``,
+},
+*/
+    ] } )
