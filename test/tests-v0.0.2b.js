289fb5d72fe3deb2aa64e1964157a40f9d9e2b45
Jerng <yangjerng@gmail.com>
2020-02-29 08:38:29 +0800

    Made Exams configurable.

--- test/tests-v0.0.2b.js
+++ test/tests-v0.0.2b.js
@@ -2,7 +2,29 @@ import * as Serl from   '../lib/serl.js'
 import * as SSON from   '../lib/sson/sson.js'
 import * as Exam from '../lib/classes/exam.js'
 
-new Exam.Exam ( { concerns : [ 
+new Exam.Exam ( { 
+    config : {
+        expand : {
+            initial_context : true,
+            tests : {
+                legibility : true,
+            }
+        }
+    },
+    concerns : [ 
+
+{   test : `Build a reactive datastore, where each datum is represented by a Proc instance.`,
+    code : function () {
+
+let n1 = new Serl.Node('Node 1')
+let p1 = n1.spawn()
+//let p2 = n2.spawn()
+console.log ( n1 )
+
+    },
+    want : 'legible'
+},
+
 
 /* Templates: 
 
