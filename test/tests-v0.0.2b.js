3ce8a10461efe45478160613dadbd2114380ce7c
Jerng <yangjerng@gmail.com>
2020-03-01 06:09:51 +0800

    Tests v0.0.2b : plan for reactive data model

--- test/tests-v0.0.2b.js
+++ test/tests-v0.0.2b.js
@@ -16,6 +16,130 @@ new Exam.Exam ( {
 {   test : `Build a reactive datastore, where each datum is represented by a Proc instance.`,
     code : function () {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************************************************************************
+ *
+ *  Abstract Data [ Model @ Graph ]
+ *
+ *  -   [ Entities @ Addresses @ Predicates ] which are spatio-temporally Discrete
+ *
+ *      1.  Data as [ Nodes @ Vertices ]
+ *
+ *      2.  [ Dependencies @ Causalities ] 
+ *              as [ Arrows @ Directed Edges] between Datum
+ *
+ *          -   The following expressions are (roughly) equivalent:
+ *
+ *              -   'Y depends on X' 
+ *              -   'X determines Y' 
+ *              -   'X supervenes on Y' 
+ *              -   'there is an arrow from X to Y' 
+ *              -   'changes in X, cause changes in Y' 
+ *              -
+ *              -   'there is a causal function, F, from X to Y'
+ *                      (X is its domain, and Y its codomain)
+ *
+ *              -   'changes in X, imply changes in Y'
+ *              -   'a change in X, necessitates an update of Y'
+ *                      (this update may, or may not, 
+                        result in the same value of Y)
+ *
+ *          -   The following expressions are (roughly) equivalent:
+ *
+ *              -   'A is equivalent to B' 
+ *              -   'there is a two-way dependency, between A and B'
+ *              -   'there are two opposing arrows, between A and B'
+ *              -   'a change in A necessites an update of B, and, 
+ *                      a change in B necessitates an update of A'
+ *
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ *  -   Existential Quantification of a Discrete Entity.
+ *
+ *      1.  [ Creation @ Existentialisation ]
+ *
+ *      2.  [ Deletion @ Uncreation @ Destruction]
+ *
+ *  -   [ Qualifying @ Copying ] Operations may be performed,
+ *          only between two Entities which Exist
+ *
+ *      3a. [ Reading @ Evaluation @ Getting ] is a copying of values,
+ *              from a remote Address (typically a Machine), 
+ *              to a local Address (typically a User).
+ *
+ *      3b. [ Updating @ Writing @ Setting @ Modifying ] is a copying of values,
+ *              from a local Address (typically a User),
+ *              to a remote Address (typically a Machine).
+ *
+ *  -   The acronym, CRUD ( Create, Read, Update, Delete), is conventional.
+ *  
+ *  -   Reading and Updating are basically the same operation, 
+ *          but in different directions.
+ *
+ *  -   Given two types of Entity, and three types of event, we should need
+ *          six ( 6 = 2 * 3 ) blocks of code to implement this model.
+ *          Additionally, we have to pipe the Qualifying Operation to two sets
+ *          of syntax, respectively for 3a. and 3b.
+*
+ ******************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let n1 = new Serl.Node('Node 1')
 let p1 = n1.spawn()
 //let p2 = n2.spawn()
@@ -43,3 +167,19 @@ console.log ( n1 )
 },
 */
     ] } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
