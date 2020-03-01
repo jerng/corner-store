38de5071a6e5fbe4161d9a2563c2d838467731e4
Jerng <yangjerng@gmail.com>
2020-03-02 00:24:18 +0800

    Begin building reactive data store.

--- test/tests-v0.0.2b.js
+++ test/tests-v0.0.2b.js
@@ -16,33 +16,13 @@ new Exam.Exam ( {
 {   test : `Build a reactive datastore, where each datum is represented by a Proc instance.`,
     code : function () {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*******************************************************************************
  *
  *  Abstract Data [ Model @ Graph ]
  *
  *  -   [ Entities @ Addresses @ Predicates ] which are spatio-temporally Discrete
  *
- *      1.  Data as [ Nodes @ Vertices ]
+ *      1.  [ Data @ Values ] as [ Nodes @ Vertices ]
  *
  *      2.  [ Dependencies @ Causalities ] 
  *              as [ Arrows @ Directed Edges] between Datum
@@ -61,7 +41,7 @@ new Exam.Exam ( {
  *              -   'changes in X, imply changes in Y'
  *              -   'a change in X, necessitates an update of Y'
  *                      (this update may, or may not, 
-                        result in the same value of Y)
+ *                       result in the same value of Y)
  *
  *          -   The following expressions are (roughly) equivalent:
  *
@@ -71,19 +51,6 @@ new Exam.Exam ( {
  *              -   'a change in A necessites an update of B, and, 
  *                      a change in B necessitates an update of A'
  *
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
  *  -   Existential Quantification of a Discrete Entity.
  *
  *      1.  [ Creation @ Existentialisation ]
@@ -103,49 +70,150 @@ new Exam.Exam ( {
  *
  *  -   The acronym, CRUD ( Create, Read, Update, Delete), is conventional.
  *  
- *  -   Reading and Updating are basically the same operation, 
- *          but in different directions.
+ *  -   (Discuss) Reading and Updating are basically the same operation, but in
+ *      different directions. The main difference is that because the flow
+ *      teleology is User-centric (describes User intent), there arises an
+ *      asymmetry between Reading and Updating, whereby 
  *
- *  -   Given two types of Entity, and three types of event, we should need
- *          six ( 6 = 2 * 3 ) blocks of code to implement this model.
- *          Additionally, we have to pipe the Qualifying Operation to two sets
- *          of syntax, respectively for 3a. and 3b.
-*
+ *      -   Reading ASSUMES the staleness of the User's data, and therefore
+ *          performs dependency check and reevaluation, of the remote data,
+ *          whereas,
+ *
+ *      -   Updating ASSUMES the staleness of the remote data, and therefore
+ *          performs no dependency check and reevaluation of the local data,
+ *
+ *  -   (Discuss) Creating may be viewed simply as the first Update event. 
+ *
+ *  -   (Sketch) Given two types of Entity, and three types of event, we should need
+ *      six ( 6 = 2 * 3 ) blocks of code to implement this model.
+ *      Additionally, we have to pipe the Qualifying Operation to two sets
+ *      of syntax, respectively for 3a. and 3b.
+ *
  ******************************************************************************/
 
 
+        let n1 = new Serl.Node('Node 1')
+        //let p1 = n1.spawn()
+        //let p2 = n2.spawn()
+        //console.log ( n1 )
+
+        let graph = {
+            vertices : {},  // consider upgrade to WeakMap (TODO)
+            arrows : {}    
+        }
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //      1.  Approach using 'a prop key' as the datum key, then defining
+    //          properties on the value.
+    //    
+    //          This however, requires that we coerce all primitives 'string's,
+    //          'boolean's, and 'number's to objects.  This would result in
+    //          large performance losses under the hood, and users may not know
+    //          why their data operations are so non-performant.
+    //          
+    //          But it completely fails for 'undefined' and 'null'. So we cannot
+    //         
+    //  Object.defineProperty ( graph.vertices, 'a prop key', {
+    //      value       : new String ('a prop value'),
+    //                              // specification default : undefined
+    //      configurable: false,    // specification default : false    
+    //      enumerable  : false,    // specification default : false    
+    //      writable    : true      // specification default : false     
+    //  } )
+    //  Object.defineProperty ( graph.vertices['a prop key'], 'a meta prop key', {
+    //      value       : 'a meta prop value',
+    //      configurable: false,    
+    //      enumerable  : false,    
+    //      writable    : true      
+    //  } )
+    //
+    //  console.log ( graph.vertices['a prop key'] ) 
+    //
+    /////////////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////////////
+    //
+    //      2.  Approach with an extra layer of indirection. 'a prop key' now
+    //          refers to an object. The value of 'a prop key' is stored in a
+    //          subprop called 'datum' because we don't want to be confusing
+    //          and name the prop 'value'. We might alternatively call it 'real
+    //          value', 'really', or 'actually'...
+    //
+
+        graph.vertices['a prop key'] = {}
+
+        Object.defineProperty ( graph.vertices['a prop key'], 'datum', {
+            configurable: false,    // specification default : false    
+            enumerable  : false,    // specification default : false    
+            writable    : true,     // specification default : false     
+            value       : 'a prop value',
+                                    // specification default : undefined
+        } )
+
+    //          As this seems to be satisfactory so far, we proceed to add a
+    //          log to this vertice. Upon its creation, a vertice can log its
+    //          first update. Upon deletion, a vertice can log a soft-delete
+    //          date.
+    //              
+    //          As this seems to be satisfactory so far, we proceed to a cache
+    //          to this vertice. The cache has a log, enabling playback of a
+    //          datum's history - if logging is enabled, this might eat up
+    //          memory very quickly with frequently modified datum, such as UI
+    //          data.
+
+        Object.defineProperty ( graph.vertices['a prop key'], 'log', {
+            configurable : false,    
+            enumerable   : false,    
+            writable     : true,     
+            value        : {
+                reads   : [],       // elements: new Date
+                updates : [],       // elements: [ new Date, datum ]
+                deletes : []        // elements: [ new Date, datum ]
+            },
+        } )
+
+    //          Assuming that all is well with the first two props, we now
+    //          can consider adding a cache. But since the 'datum's have not
+    //          been described to hold computed values, the computation time of
+    //          any datum should be 0, and so a cache would be pointles. First
+    //          we should add computability of values.
+
+        Object.defineProperty ( graph.vertices['a prop key'], 'computation', {
+            configurable : false,    
+            enumerable   : false,    
+            writable     : true,     
+            value        : () => 1 + 2   // some kind of function
+        } )
+
+    //          This suffices for computations which do not depend on other
+    //          data. In order to point to other data in the graph, we need to
+    //          start storing arrows between vertices.
+
+        console.log ( graph.vertices['a prop key'] )
 
 
+        /*
+        // example where Property Descriptor is a Data Descriptor
+        key1 : Object.defineProperty ( {}, {
+            cache : {           // the prop's key/name
+                value : {       // the prop's value
+                    value : 'some arbitrary value',
+                    log : {
+                        updates : [],
+                        hits : [],
+                        misses : []
+                    }
+                },
+                configurable : false,
+                enumerable : false,
+                writable : false
+            },
+        // example where Property Descriptor is an Accessor Descriptor
+        } )
+        */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let n1 = new Serl.Node('Node 1')
-let p1 = n1.spawn()
-//let p2 = n2.spawn()
-console.log ( n1 )
-
-    },
+    }, // code
     want : 'legible'
 },
 
